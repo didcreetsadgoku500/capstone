2b65a9b5b6a1e71ea16939060a040dba145f4a91
Alex Englert <alex.englert.22@cnu.edu>
2024-09-30 10:48:38 -0400

    Add console logs to debug Authjs sessions

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -4,11 +4,22 @@ import Osu from "next-auth/providers/osu"
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Osu],
   callbacks: {
-    jwt({ token, session, account, user}) {
-      return {...token, session, account, user}
+    jwt({ token, session, account, user, trigger, profile}) {
+      console.log('jwt callback', {
+        token,
+        trigger,
+        profile,
+        user,
+        session,
+        account
+      });
+      return token
     },
     session({ session, token }) { // TODO: Whatever isnt working here.Need full access token
-      session.user.id = token.id
+      console.log('session callback', {
+        token,
+        session,
+      });
       return session
     }
   }

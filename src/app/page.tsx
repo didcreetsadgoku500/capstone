36795e72b5fcf06db8af54d3c29bb21225608f11
Alex Englert <alex.englert.22@cnu.edu>
2024-10-11 02:06:53 -0400

    Replace test button with shadcn component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,9 +27,7 @@ export default async function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <SignInButton
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-          />
+          <SignInButton />
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
--- src/app/signInButton.tsx
+++ src/app/signInButton.tsx
@@ -1,22 +1,20 @@
 "use client"
 import { signIn } from "next-auth/react"
 import { useState } from "react"
+import { Button } from "@/components/ui/button"
+import { LoaderCircle } from "lucide-react"
  
-interface SignInButtonProps {
-    className: string
-}
-
-
-export default function SignInButton({ className }: SignInButtonProps) {
+export default function SignInButton() {
   const [isLoading, setLoading] = useState(false);
 
   return (
-  <button className={className} onClick={() => {
+    <Button disabled={isLoading} onClick={() => {
       signIn("osu", {redirect: true})
       setLoading(true)
-
-  }}>
-    j
-    </button>
+    }}
+      
+    >
+      {isLoading ? <LoaderCircle strokeWidth={3} className="animate-spin mx-3" /> : "Sign In"} 
+    </Button>
     )
 }

a490b0b79545ddfd41b5c21595ff0aa9c1c828f2
Alex Englert <alex.englert.22@cnu.edu>
2024-10-18 13:50:02 -0400

    Change font to inter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import SignInButton from "./signInButton";
+import SignInButton from "../components/signInButton";
 import { auth } from "@/utils/auth";
 
 
@@ -9,7 +9,7 @@ export default async function Home() {
   const session = await auth()
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -19,7 +19,7 @@ export default async function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left ">
           <li className="mb-2">
               Welcome {session?.user.username}!
           </li>

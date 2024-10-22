b8a261953728727f97facb788fff8658982953b7
Alex Englert <alex.englert.22@cnu.edu>
2024-10-22 13:08:09 -0400

    Proppify tournament card

--- src/app/listing/tournamentCard.tsx
+++ src/app/listing/tournamentCard.tsx
@@ -1,14 +1,22 @@
 import { Button } from "@/components/ui/button";
 
-const tempSrc = "https://raw.githubusercontent.com/didcreetsadgoku500/tournament.sh/refs/heads/main/src/assets/strafeSmoke.jpg?token=GHSAT0AAAAAACX7YTQR7HVEF55W3IWL4Q2UZYWUQEA"
 
-export default function TournamentCard({ src = tempSrc, title = "Tournament Title", blurb = "This is a long blurb that will be truncated with an ellipsis if it exceeds the available space in thethat will be truncated with an ellipsis if it exceeds the available space in thethat will be truncated with an ellipsis if it exceeds the available space in thethat will be truncated with an ellipsis if it exceeds the available space in thethat will be truncated with an ellipsis if it exceeds the available space in thethat will be truncated with an ellipsis if it exceeds the available space in the card." }) {
+
+interface TournamentCardProps {
+  title: string,
+  description: string,
+  bannerSrc: string,
+  tournamentId: number,
+  action: "register" | "unregister"
+}
+
+export default function TournamentCard({ bannerSrc, title, description, action = "register"}: TournamentCardProps) {
     return (
       <div className="border rounded-lg p-3 flex flex-col w-[300px] h-60 hover:shadow-md">
-        <img className="w-full h-20 object-cover rounded-lg" src={src} alt="Tournament Banner" />
+        <img className="w-full h-20 object-cover rounded-lg" src={bannerSrc} alt="Tournament Banner" />
         <div className="flex flex-col flex-grow overflow-hidden mt-2">
           <h3 className="text-md font-medium">{title}</h3>
-          <p className="text-xs font-regular text-primary/75 line-clamp-3">{blurb}</p>
+          <p className="text-xs font-regular text-primary/75 line-clamp-3">{description}</p>
         </div>
         <div className="flex justify-end mt-2">
           <Button size="sm">Register</Button>
--- src/app/listing/tournamentsTableView.tsx
+++ src/app/listing/tournamentsTableView.tsx
@@ -3,6 +3,15 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import TournamentCard from "./tournamentCard";
 //
+const tempSrc = "https://raw.githubusercontent.com/didcreetsadgoku500/tournament.sh/refs/heads/main/src/assets/strafeSmoke.jpg?token=GHSAT0AAAAAACX7YTQR7HVEF55W3IWL4Q2UZYWUQEA"
+
+const sampleTournament =  {
+    title: "My Tournament",
+    description: "I love my tournament",
+    bannerSrc: tempSrc,
+    tournamentId: 18734265876
+
+}
 
 export default function TournamentsTableView() {
     return (
@@ -14,12 +23,7 @@ export default function TournamentsTableView() {
             </div>
             <div className="flex flex-wrap gap-3 justify-center">
                 
-               <TournamentCard />
-               <TournamentCard />
-               <TournamentCard />
-               <TournamentCard />
-               <TournamentCard />
-               <TournamentCard />
+               <TournamentCard title={sampleTournament.title} description={sampleTournament.description} bannerSrc={sampleTournament.bannerSrc} tournamentId={sampleTournament.tournamentId}/>
             </div>
 
 

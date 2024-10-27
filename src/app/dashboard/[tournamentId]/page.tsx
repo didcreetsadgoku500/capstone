dbe8c36f26286dbf2029654f685f26b723a418ad
Alex Englert <alex.englert.22@cnu.edu>
2024-10-27 12:58:01 -0400

    Refactor Dashboard

--- src/app/dashboard/[tournamentId]/page.tsx
+++ src/app/dashboard/[tournamentId]/page.tsx
@@ -1,3 +1,4 @@
+import { Card } from "@/components/ui/card";
 import { auth } from "@/lib/auth";
 import { verifyRole } from "@/lib/permissions";
 
@@ -6,17 +7,30 @@ export default async function Page({ params }: { params: { tournamentId: string 
     const session = await auth();
 
     if (!session || !session.user.id) {
-        return <div className="mx-auto">You are not signed in</div>
+        return <Unauthenticated />
     }
     const permission = await verifyRole(session.user.id, `tournament-${params.tournamentId}`, "host")
     if (!permission) {
-        return (<div className="mx-auto">Missing required permissions for tournament ID {params.tournamentId}. </div>)
+        return <Unauthorized tournamentId={params.tournamentId}/>
     }
 
     return <div className="mx-auto">
+        <Card>
+
             Dashboard access granted for tournament ID {params.tournamentId}
             <br /><br />
             TODO: Make dashboard
+        </Card>
         </div>
+        
 
   }
+
+
+function Unauthenticated() {
+    return <div className="mx-auto">You are not signed in</div>
+  }
+
+
+function Unauthorized({tournamentId}: {tournamentId: string}) {
+    return (<div className="mx-auto">Missing required permissions for tournament ID {tournamentId}. </div>)  }

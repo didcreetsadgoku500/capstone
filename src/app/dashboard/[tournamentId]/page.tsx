09f40c7c6725e15415b973440ebf859b5b712e84
Alex Englert <alex.englert.22@cnu.edu>
2024-10-27 15:04:21 -0400

    Start fleshing out dashboard

--- src/app/dashboard/[tournamentId]/page.tsx
+++ src/app/dashboard/[tournamentId]/page.tsx
@@ -1,4 +1,8 @@
-import { Card } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import { Textarea } from "@/components/ui/textarea";
 import { auth } from "@/lib/auth";
 import { verifyRole } from "@/lib/permissions";
 
@@ -14,12 +18,55 @@ export default async function Page({ params }: { params: { tournamentId: string 
         return <Unauthorized tournamentId={params.tournamentId}/>
     }
 
-    return <div className="mx-auto">
-        <Card>
+    return <div className="mx-auto max-w-screen-xl w-full">
+        <Card className="max-w-screen-xl grid grid-cols-4">
+            <div className="col-span-1">
 
-            Dashboard access granted for tournament ID {params.tournamentId}
-            <br /><br />
-            TODO: Make dashboard
+            <CardHeader>
+                <CardTitle>Navigate</CardTitle>
+            </CardHeader>
+            <CardContent>
+
+            </CardContent>
+            </div>
+            <div className="col-span-3">
+
+            <CardHeader>
+                <CardTitle>Dashboard</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <div className="grid w-full items-center gap-4">
+
+                    <div>
+                        <Label htmlFor="name">Tournament Name</Label>
+                        <Input id="name" placeholder="Name of your tournament" />
+                    </div>
+
+                    <div>
+                        <Label htmlFor="desc">Description</Label>
+                        <Textarea id="desc" placeholder="Describe your tournament" />
+                    </div>
+
+                    <div>
+                        <Label htmlFor="min">Rank Range</Label>
+                        <div className="flex flex-row items-center max-w-96 w-full justify-between">
+
+                            <Input  id="min" placeholder="Min" type="number" step={1}/>
+                            <span className="mx-3"> - </span>
+                            <Input id="min" placeholder="Max" type="number" step={1}/>
+                        </div>
+                    </div>
+
+                    <div className="flex items-center max-w-96 w-full justify-between">
+                        <Label htmlFor="bws">Use Badge Weighted Seeding</Label>
+                        <Switch id="bws"/>
+                    </div>
+
+
+                </div>
+            </CardContent>
+            </div>
+            
         </Card>
         </div>
         

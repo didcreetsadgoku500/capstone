31d6773d030e546999a139d83e9fb5f87d712d60
Alex Englert <alex.englert.22@cnu.edu>
2024-10-28 17:15:46 -0400

    Add visibility to tournament schema

--- src/app/listing/page.tsx
+++ src/app/listing/page.tsx
@@ -1,9 +1,16 @@
+import prisma from "@/lib/db";
 import Filters from "./filters";
 import TournamentsTableView from "./tournamentsTableView";
+import { Visibility } from "@prisma/client";
 
 
-export default function ListingsPage() {
-
+export default async function ListingsPage() {
+    const tournaments = await prisma.tournament.findMany({
+        where: {
+            visibility: Visibility.PUBLIC
+        },
+        take: 20
+    })
     
     return (
         <div className="max-w-screen-xl w-full mx-auto grid grid-cols-4 gap-6">

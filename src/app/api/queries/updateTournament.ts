470b7c42daf65aaefc6caedbea4ba4a218bdde03
Alex Englert <alex.englert.22@cnu.edu>
2024-11-03 19:05:45 -0500

    Add authorization to updateTournament

--- src/app/api/queries/updateTournament.ts
+++ src/app/api/queries/updateTournament.ts
@@ -1,9 +1,16 @@
 "use server"
 
+import { auth } from "@/lib/auth";
 import prisma from "@/lib/db"
+import { verifyRole } from "@/lib/permissions";
 import { Tournament } from "@prisma/client";
 
 export default async function updateTournament(tournamentId: bigint, data: Partial<Tournament>) {
+    
+    const session = await auth()
+
+    const role = await verifyRole(session?.user.id, `tournament-${tournamentId}`, "host")
+
     await prisma.tournament.update({
         where: {
             tournamentId

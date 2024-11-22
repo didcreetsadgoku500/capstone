b0b6c96617b1dfe99999e4e94da4ca7e0032c61f
Alex Englert <alex.englert.22@cnu.edu>
2024-11-21 23:59:26 -0500

    Add staff edit button to tournament listing

--- src/app/listing/[tournamentId]/page.tsx
+++ src/app/listing/[tournamentId]/page.tsx
@@ -5,8 +5,9 @@ import RegisterButton from "@/components/registerButton"
 import { pStyles, smallStyles } from "@/components/textStyles"
 import { auth } from "@/lib/auth"
 import { rankFormatter } from "@/lib/helper"
-import { verifyRole } from "@/lib/permissions"
-import { Gamemode, Tournament } from "@prisma/client"
+import { Gamemode } from "@prisma/client"
+import { Edit } from "lucide-react"
+import Link from "next/link"
 
 export default async function Page({ params }: { params: { tournamentId: string } }) {
   const tournamentId = BigInt((await params).tournamentId)
@@ -34,12 +35,12 @@ export default async function Page({ params }: { params: { tournamentId: string 
 
 
 
-
   return (<div className={"flex flex-row justify-between gap-4"}>
     <div className={pStyles}>
 
       {tournament?.tourDesc}
     </div>
+    <div className="flex flex-col gap-2">
     <div className={smallStyles + " bg-zinc-100 flex flex-col gap-2 p-2 rounded-md h-fit"}>
       <div className="flex flex-row w-48 justify-between">Gamemode <img src={icons[tournament.gamemode]} className="brightness-0 h-4 w-4" /></div>
       <div className="flex flex-row w-48 justify-between">{tournament.usesBWS ? "Uses BWS" : "No BWS"}</div>
@@ -51,7 +52,17 @@ export default async function Page({ params }: { params: { tournamentId: string 
       </div>
 
       <RegisterButton details={tournament} isDefaultRegistered={isRegistered} />
+      </div>
+      {
+        isStaff ? <Link href={`/dashboard/${tournamentId}`}
+          className="flex flex-row justify-center border-dashed border-2 rounded-md border-blue-200 hover:border-blue-300 transition-colors text-sm p-1 bg-blue-100/25"
+        >
+            
+            Edit Tournament <Edit className="ml-2" />
+          </Link> : ""
+      }
     </div>
+    
 
   </div>
   )

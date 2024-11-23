8499a6cf723296446b981bcf6abcda6da9215040
Alex Englert <alex.englert.22@cnu.edu>
2024-11-23 00:18:02 -0500

    Add draft label to nonpublic tournaments

--- src/app/listing/[tournamentId]/header.tsx
+++ src/app/listing/[tournamentId]/header.tsx
@@ -18,7 +18,10 @@ export async function DetailsHeader({params}: {params: {tournamentId: string}}) 
     return (<>
 
         <img className="rounded-md aspect-banner object-cover mb-3" src={tournamentDetails?.bannerUrl} />
-        <h2 className={h3Styles}>{tournamentDetails?.tourName}</h2>
+        <div className="flex flex-row justify-between">
+
+        <h2 className={h3Styles}>{tournamentDetails?.tourName}</h2> <h3 className={"text-2xl font-semibold text-primary/50"}>{!tournamentDetails?.public && "Draft"}</h3>
+        </div>
     </>
     )
 }

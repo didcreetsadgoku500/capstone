fb0434f9e80aba196b73beb82153f9cc6cbaae27
Alex Englert <alex.englert.22@cnu.edu>
2024-11-24 02:16:18 -0500

    Styling changes to matchlistitem

--- src/app/dashboard/[tournamentId]/matches/matchListItem.tsx
+++ src/app/dashboard/[tournamentId]/matches/matchListItem.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
-import { Match } from "@prisma/client"
-import { User, UserRound } from "lucide-react";
+import { Match, MatchStatus } from "@prisma/client"
+import { Edit, User, UserRound } from "lucide-react";
 import { UserCompact } from "osu-web.js"
 
 // i think the play is just have an edit match button that callsback to dashboardclient face so registrants doesnt get passed down
@@ -17,9 +17,9 @@ export default function MatchListItem({ match, users }: { match: Match, users: U
     max-h-24 hover:max-h-48 focus:max-h-48
     flex-col xl:flex-row overflow-clip xl:items-center">
 
-        <div className="flex flex-row border rounded-md p-4 items-center justify-between max-w-screen-sm w-full bg-primary-foreground z-10">
+        <div className="flex flex-row border rounded-md p-2 items-center justify-between max-w-screen-sm w-full bg-primary-foreground z-10">
             <div className="flex-grow basis-0">
-                <div className="flex flex-row items-center">
+                <div className="flex flex-row items-center p-2">
 
 
                     {team1 ? <img className="w-12 h-12 rounded-full" src={team1?.avatar_url} /> : <UserRound className="w-12 h-12 rounded-full" />}
@@ -30,9 +30,22 @@ export default function MatchListItem({ match, users }: { match: Match, users: U
             </div>
 
 
-            <div className="mx-2">VS</div>
+            <div className="flex-col flex items-center">
+                <div>
+                    {statusText(match.matchStatus)}
+                    </div>
+                <div className="font-bold text-primary/50">
+                    VS
+                    </div>
+                    <div className="text-primary/50 items-baseline mt-1">
+
+                    {match.matchDateTime ? match.matchDateTime.toUTCString() : "Unscheduled"}
+                    </div>
+
+            
+            </div>
             <div className="flex-grow basis-0">
-                <div className="flex flex-row-reverse items-center">
+                <div className="flex flex-row-reverse items-center p-2">
 
 
                     {team2 ? <img className="w-12 h-12 rounded-full" src={team2?.avatar_url} /> : <UserRound className="w-12 h-12 rounded-full" />}
@@ -48,9 +61,24 @@ export default function MatchListItem({ match, users }: { match: Match, users: U
         <div className="
         -mt-4 group-hover:mt-0  group-focus:mt-0 group-active:mt-0
         xl:-ml-64 xl:group-hover:ml-0  xl:group-focus:ml-0 xl:mt-0 transition-all p-2 flex opacity-0 group-hover:opacity-100 group-focus:opacity-100 group-active:opacity-100 xl:items-center">
-            <Button variant={"ghost"} className="text-primary-foreground"   >Edit Match</Button>
+            <Button variant={"ghost"} className="text-primary-foreground"   >Edit <Edit className="w-4 h-4 ml-1"/></Button>
             </div>
             </div>
         
     </div>
+}
+
+function statusText(status: MatchStatus) {
+    if (status == MatchStatus.NOT_STARTED) {
+        return "Not Started"
+    }
+    if (status == MatchStatus.ENDED) {
+        return "Completed"
+    }
+    if (status == MatchStatus.IN_PROGRESS) {
+        return "In Progress"
+    }
+    if (status == MatchStatus.PAUSED) {
+        return "Paused"
+    }
 }

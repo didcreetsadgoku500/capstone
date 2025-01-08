5ab1d7816c5a4d91178662d913ef2148514848ae
= <alex.englert.22@cnu.edu>
2025-01-07 20:35:53 -0500

    Fix type signatures on userId in auth config

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,45 +1,22 @@
 import NextAuth, {Profile, type DefaultSession} from "next-auth"
-import Osu from "next-auth/providers/osu"
-import { narrowProfile } from "./helper"
+import Osu, { OsuUserCompact } from "next-auth/providers/osu"
 import { JWT } from "next-auth/jwt"
-
-export interface UserStatistics {
-  global_rank: number
-}
-
-export type ProfileData = {
-    id?: string | null,
-    username: string,
-    avatar_url: string,
-    country_code: string,
-    country: string,
-    discord: string,
-    is_restricted?: boolean,
-    statistics_rulesets: {
-      osu: UserStatistics,
-      taiko: UserStatistics,
-      fruits: UserStatistics,
-      mania: UserStatistics
-  }
-}
-
-
 
 declare module "next-auth" {
   /**
    * Returned by `auth`, extends session object
    */
   interface Session {
-    user: ProfileData
+    user: OsuUserCompact
     access_token: string
   }
 
-  interface Profile extends ProfileData {}
+  interface Profile extends OsuUserCompact {}
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    user: ProfileData;
+    user: OsuUserCompact;
     access_token: string
   }
 }
@@ -59,9 +36,11 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         token.access_token = account.access_token
       }
 
-      if (profile) {
-        profile = narrowProfile(profile);
-        token.user = profile;
+      if (profile && profile.id) {
+        token.user = {
+          ...profile,
+          id: Number(profile.id)
+        };
       }
       return token
     },
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,40 +1,8 @@
 import { MatchStatus } from "@prisma/client";
-import { ProfileData } from "./auth";
-import { Profile } from "next-auth"
 
 
-export function narrowProfile(profile: Profile): ProfileData {
-  const result: ProfileData = {
-    id: profile.id,
-    username: profile.username,
-    avatar_url: profile.avatar_url,
-    country_code: profile.country_code,
-    country: profile.country,
-    discord: profile.discord,
-    is_restricted: profile.is_restricted,
-    statistics_rulesets: {
-      osu: {
-        global_rank: profile.statistics_rulesets.osu.global_rank
-      },
-      taiko: {
-        global_rank: profile.statistics_rulesets.taiko.global_rank
 
-      },
-      fruits: {
-        global_rank: profile.statistics_rulesets.fruits.global_rank
-
-      },
-      mania: {
-        global_rank: profile.statistics_rulesets.mania.global_rank
-
-      }
-    }
-  }
-
-  return result;
-}
-
-export function onlyUnique(value, index, array) {
+export function onlyUnique(value: any, index: number, array: any[]) {
   return array.indexOf(value) === index;
 }
 

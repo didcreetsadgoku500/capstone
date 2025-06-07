5a21244ce72333422d2bb2c63e18c7ec652a514f
= <alex.englert.22@cnu.edu>
2025-06-07 16:24:47 -0400

    Move dashboard sidebar to components folder

--- src/app/dashboard/[tournamentId]/layout.tsx
+++ src/app/dashboard/[tournamentId]/layout.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Sidebar } from "./sidebar";
+import { Sidebar } from "@/components/dashboard/sidebar";
 
 
 

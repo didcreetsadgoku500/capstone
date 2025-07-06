602a16612b2d5ca362d8f4b6dd0a3a84fca18edf
= <alex.englert.22@cnu.edu>
2025-07-06 11:59:42 -0400

    Fix navbar import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from 'next/font/google'
-import { NavBar } from "@/components/navBar";
+import { NavBar } from "@/components/navbar/navBar";
 import { Toaster } from "@/components/ui/toaster";
 
 
